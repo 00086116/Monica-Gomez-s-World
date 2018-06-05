0b7190bc2b5e3fe2f87b83753166a1b840cb21cb
Monica Gomez <Monica Gomez@192.168.2.13>
2018-06-05 00:17:37 -0600

    Creacion de clases.

--- src/Edificaciones/ConstruirDragones.java
+++ src/Edificaciones/ConstruirDragones.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Mundo;
+package Edificaciones;
 
 /**
  *
  * @author Monica Gomez
  */
-public class MundoM {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ConstruirDragones {
     
 }
