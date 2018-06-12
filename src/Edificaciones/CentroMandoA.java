ed60eddc04084fbed055b297da5972e6ff3b82dc
Monica Gomez <Monica Gomez@192.168.2.13>
2018-06-11 23:57:12 -0600

    Probando repo

--- src/Edificaciones/CentroMandoA.java
+++ src/Edificaciones/CentroMandoA.java
@@ -1,14 +1,18 @@
 package Edificaciones;
+import jugadores.Player1;
+import locacion;
+
+
     
 public class CentroMandoA extends Locacion {
 
-	public CentroMandoA(Player1 p,Inventory i) {
-		super.locationId=1;
+	public CentroMandoA(Player1 p,Recurso1 i) {
+		super.locacionId=1;
 		super.name="Centro de Mando de la raza agua";
 		super.itemName="Fire Wood";
-		super.obstacle=new Vampire();
-		super.player=p;
-		super.inventory=i;
+		super.obstacle=new Agua();
+		super.player1=p;
+		super.Recurso1=i;
 		
 	}
 }
--- src/Recursos/Recurso1.java
+++ src/Recursos/Recurso1.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Recursos;
-
-/**
- *
- * @author Monica Gomez
- */
 public class Recurso1 {
-    
+	public boolean water,food,fireWood=false;
+	public int id,weaponID,armorID;
+	public int defend=0;
+	public String weaponString;
+	public String armorString;
+	public void addWeapon(int id , String name , int damage)
+	{
+		this.weaponID=id;
+		this.weaponString=name + " " + "+" + damage +" damage";
+		
+	}
+	
+	public void addArmor(int id ,String name , int avoid)
+	{
+		this.armorID=id;
+		this.armorString="Armor: " + name + " Armor " + avoid + " avoid.";
+		
+	}
+	public void defend(int avoid)
+	{
+		defend=avoid;
+	}
+	public int getDefend()
+	{
+		return defend;
+	}
+	
+	public boolean isWater() {
+		return water;
+	}
+	public void setWater(boolean water) {
+		this.water = water;
+	}
+	public boolean isFood() {
+		return food;
+	}
+	public void setFood(boolean food) {
+		this.food = food;
+	}
+	public boolean isFireWood() {
+		return fireWood;
+	}
+	public void setFireWood(boolean fireWood) {
+		this.fireWood = fireWood;
+	}
 }
